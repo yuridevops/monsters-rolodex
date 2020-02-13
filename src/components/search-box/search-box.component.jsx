661b91e01ba72aad816c7017958df706b3efeb12
Marcos Yuri Rosa Junior <marcosyuri.fsz@gmail.com>
2020-02-13 16:23:13 -0300

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './search-box.styles.css'
+
+export const SearchBox = ({placeholder, handleChange}) => 
+<input type="search" placeholder={placeholder} className='search'
+onChange = {handleChange}
+
+>
+</input>
